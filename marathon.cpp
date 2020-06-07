73dc37c9cd993229722c7dd736662f266c7a3b55
YooJ-K <55521930+YooJ-K@users.noreply.github.com>
2020-06-07 19:02:20 +0900

    another marathon

--- marathon.cpp
+++ marathon.cpp
@@ -1,56 +1,39 @@
 #include <iostream>
 #include <fstream>
-#include <list>
-#include <algorithm>
 using namespace std;
 
-typedef struct info {
-	int start, end, weight;
-}info;
+#define INF 1000000;
 
-int k;
-list<info> v; // v의 정보
-info t;
-
-bool isSame(info &a) {
-	if (t.end == a.end && t.start == a.start) return true;
-	else return false;
-}
-
-bool comp(info &a, info &b) {
-	return a.weight < b.weight;
-}
+int k, v[51][51];
 
 int main() {
-	int s, e, w;
-	list<info>::iterator iter;
-
 	ifstream in("marathon.inp");
-	in >> k;
+	ofstream out("marathon.out");
+	int s, e;
+	
 
+	in >> k;
 	for (int i = 1; i <= k; i++) {
 		in >> s;
 		for (int j = 1; j <= k; j++) {
+			if (i == j) v[i][j] = 0;
+			else v[i][j] = INF;
+		}
+		for (int j = 1; j <= k; j++) {
 			in >> e;
 			if (e == 0) break;
-			in >> w;
-			t = { e, s, w };
-			iter = find_if(v.begin(), v.end(), isSame);
-			if (iter == v.end()) {
-				t = { s, e, w };
-				v.push_back(t);
-			}
+			in >> v[s][e];
+			v[e][s] = v[s][e];
 		}
 	}
 
 	in.close();
 
-	v.sort(comp);
-
-	ofstream out("marathon.out");
-
-	for (iter = v.begin(); iter != v.end(); iter++) {
-		out << iter->start << " " << iter->end << " " << iter->weight << endl;
+	for (int i = 1; i <= k; i++) {
+		for (int j = 1; j <= k; j++) {
+			out << v[i][j] << " ";
+		}
+		out << endl;
 	}
 
 	out.close();
